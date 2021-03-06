d998c9608040bfc27eb45f444778b246d1c5d097
peachberry <peachberry0318@gmail.com>
2021-03-07 00:31:59 +0900

    feat(Author): 작가 구현

---
+++ src/main/java/com/library/study/demo/controller/AuthorController.java
@@ -0,0 +1,9 @@
+package com.library.study.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorController {
+//saveAuthor
+//getAuthor
+}
