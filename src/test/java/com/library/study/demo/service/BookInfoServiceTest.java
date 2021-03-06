660645000ef7dea681f3570a278635892bda3419
peachberry <peachberry0318@gmail.com>
2021-03-07 01:37:10 +0900

    feat(BookInfoServiceTest) : BookInfoServiceTest 테스트 구현

---
+++ src/test/java/com/library/study/demo/service/BookInfoServiceTest.java
@@ -0,0 +1,40 @@
+package com.library.study.demo.service;
+
+import com.library.study.demo.dao.AuthorRepository;
+import com.library.study.demo.dao.BookInfoRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class BookInfoServiceTest {
+
+    @Mock
+    private AuthorRepository authorRepository;
+
+    @Mock
+    private BookInfoRepository bookInfoRepository;
+
+    @InjectMocks
+    private BookInfoService bookInfoService;
+
+    @Test
+    @DisplayName("작가 아이디 유효성 검사")
+    void testValidAuthorId() {
+        given(authorRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        Assertions.assertThatThrownBy(() -> bookInfoService.save("id", 1L))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        verify(authorRepository, times(1)).findById(anyLong());
+    }
+}
