e08cf7d51d330a1847ee8fadd8db22935d5df85a
peachberry <peachberry0318@gmail.com>
2021-03-20 00:00:06 +0900

    feat(AuthorController) : save, getAuthor 작성

--- src/main/java/com/library/study/demo/controller/AuthorController.java
+++ src/main/java/com/library/study/demo/controller/AuthorController.java
@@ -1,9 +1,34 @@
 package com.library.study.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.library.study.demo.controller.dto.AuthorResponse;
+import com.library.study.demo.controller.dto.SaveAuthorRequest;
+import com.library.study.demo.controller.dto.SaveAuthorResponse;
+import com.library.study.demo.domain.Author;
+import com.library.study.demo.service.AuthorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/author")
 public class AuthorController {
-//saveAuthor
-//getAuthor
+
+    private AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<SaveAuthorResponse> save(@RequestBody SaveAuthorRequest saveAuthorRequest) {
+        return ResponseEntity.ok(
+                authorService.save(saveAuthorRequest.toCommand())
+        );
+    }
+
+    @GetMapping("")
+    public ResponseEntity<AuthorResponse> getAuthor(@RequestParam(name="id") Long authorId) {
+        Author author = authorService.getAuthor(authorId);
+
+        return ResponseEntity.ok(new AuthorResponse(author.getId(), author.getName()));
+    }
 }
