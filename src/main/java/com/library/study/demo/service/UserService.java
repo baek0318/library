567b1cb3c4c098e3fe9a2343c6047064dab7e718
peachberry <peachberry0318@gmail.com>
2021-04-16 23:39:27 +0900

    feat(User) : User 도메인 save 구현

--- src/main/java/com/library/study/demo/service/UserService.java
+++ src/main/java/com/library/study/demo/service/UserService.java
@@ -1,8 +1,12 @@
 package com.library.study.demo.service;
 
+import com.library.study.demo.dao.AuthorityRepository;
 import com.library.study.demo.dao.UserRepository;
+import com.library.study.demo.domain.Authority;
+import com.library.study.demo.domain.Role;
 import com.library.study.demo.domain.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -16,9 +20,11 @@ public class UserService {
         this.authorityRepository = authorityRepository;
     }
 
+    @Transactional
     public Long save(User user) {
 
-
+        Authority authority = authorityRepository.findByRole(Role.USER).orElseThrow(IllegalArgumentException::new);
+        user.setAuthority(authority);
         User saved = userRepository.save(user);
 
         return saved.getId();
